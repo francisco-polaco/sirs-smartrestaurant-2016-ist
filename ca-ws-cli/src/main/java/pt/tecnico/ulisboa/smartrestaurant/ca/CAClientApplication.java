ed751e8f78870c6ff996e0c86250b4b5a9a5d748
Francisco Santos <franciscopolaco@ist.utl.pt>
2016-11-16 21:20:05 +0000

    insectos a bailar

--- ca-ws-cli/src/main/java/pt/tecnico/ulisboa/smartrestaurant/ca/CAClientApplication.java
+++ ca-ws-cli/src/main/java/pt/tecnico/ulisboa/smartrestaurant/ca/CAClientApplication.java
@@ -2,11 +2,13 @@ package pt.tecnico.ulisboa.smartrestaurant.ca;
 
 import pt.tecnico.ulisboa.smartrestaurant.ca.ws.cli.CAClient;
 
+import java.io.IOException;
+
 /**
  * Created by xxlxpto on 06-05-2016.
  */
 public class CAClientApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("Argument(s) missing!");
             System.err.printf("Usage: java %s wsURL%n",
@@ -16,7 +18,8 @@ public class CAClientApplication {
 
 
         // Start CA
-        CAClient CAClient = new CAClient();
+        CAClient CAClient = new CAClient(args[0]);
+        System.in.read();
         //CAClient.getEntityCertificate("Broker");
     }
 }
