7ec4694c842f6947fe910a05a20bac60edb38e35
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2016-12-03 14:53:44 +0000

    script to install and run

---
+++ install.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+import time
+
+folders = ["ca-ws", "ca-ws-cli", "ws-handlers", "kitchen-smartrestaurant-ws-server", "smartrestaurant-ws", "kitchen-smartrestaurant-ws-cli", "smartrestaurant-ws-cli", "customer-smartrestaurant-ws"]
+cmds = ["mvn clean install exec:java", "mvn clean install", "mvn clean install", "mvn clean install exec:java", "mvn clean install exec:java", "mvn clean install", "mvn clean install", "mvn clean install"]
+
+for idx, folder in enumerate(folders):
+	os.chdir(folder)
+	subprocess.Popen(args=["gnome-terminal", "--command=" + cmds[idx]])
+	if folder == "smartrestaurant-ws":
+		time.sleep(15)
+	else:
+		time.sleep(5)
+	os.chdir("..")
