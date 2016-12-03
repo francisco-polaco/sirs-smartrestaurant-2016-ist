08e89834415a0d5efe79face28f721bdefc91711
Francisco Santos <franciscopolaco@ist.utl.pt>
2016-12-03 13:14:43 +0000

    chain correta

--- kitchen-smartrestaurant-ws-server/src/main/java/pt/tecnico/ulisboa/smartrestaurant/kitchenServer/ws/cli/KitchenClientServerImpl.java
+++ kitchen-smartrestaurant-ws-server/src/main/java/pt/tecnico/ulisboa/smartrestaurant/kitchenServer/ws/cli/KitchenClientServerImpl.java
@@ -9,13 +9,13 @@ import java.io.Serializable;
  * Created by tiago on 11/17/16.
  */
 @WebService(endpointInterface = "pt.tecnico.ulisboa.smartrestaurant.kitchenServer.ws.cli.KitchenClientServer")
-@HandlerChain(file= "/kitchen_client_server_handler-chain.xml")
+@HandlerChain(file= "/kitchen_handler-chain.xml")
 public class KitchenClientServerImpl implements KitchenClientServer, Serializable{
     private ArrayList<Long> _list = new ArrayList<Long>();
 
     @Override
     public void addList(long id){
-        _list.add(new Long(id));
+        _list.add(id);
         System.out.println("Novo pedido id "+id);
     }
 
