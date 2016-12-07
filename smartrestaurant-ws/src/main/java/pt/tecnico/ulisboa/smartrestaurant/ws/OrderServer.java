6fe773dec942e077c8a017bb994ae345676cbb04
Francisco Santos <franciscopolaco@ist.utl.pt>
2016-12-07 16:26:28 +0000

    tomcat

--- smartrestaurant-ws/src/main/java/pt/tecnico/ulisboa/smartrestaurant/ws/OrderServer.java
+++ smartrestaurant-ws/src/main/java/pt/tecnico/ulisboa/smartrestaurant/ws/OrderServer.java
@@ -1,5 +1,6 @@
 package pt.tecnico.ulisboa.smartrestaurant.ws;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 /**
@@ -7,22 +8,22 @@ import javax.jws.WebService;
  */
 @WebService
 public interface OrderServer {
-    String ping(String msg);
+    @WebMethod String ping(String msg);
 
-    void registerNewUser(String username, String password, String firstName, String lastName, int nif);
+    @WebMethod void registerNewUser(String username, String password, String firstName, String lastName, int nif);
 
-    byte[] login(String username, String passwordSha2Hash, int tableNo, int OTP);
+    @WebMethod byte[] login(String username, String passwordSha2Hash, int tableNo, int OTP);
 
-    String requestMyOrdersProducts(byte[] sessionId);
+    @WebMethod String requestMyOrdersProducts(byte[] sessionId);
 
-    void addProductToOrder(byte[] sessionId, String productName);
+    @WebMethod void addProductToOrder(byte[] sessionId, String productName);
 
-    void orderProducts(byte[] sessionId, String passwordSha2Hash);
+    @WebMethod void orderProducts(byte[] sessionId, String passwordSha2Hash);
 
-    void confirmPayment(byte[] sessionId, String passwordSha2Hash, String paypalReference);
+    @WebMethod void confirmPayment(byte[] sessionId, String passwordSha2Hash, String paypalReference);
 
-    double getPaymentDetails(byte[] sessionId);
+    @WebMethod double getPaymentDetails(byte[] sessionId);
 
-    void logOut(byte [] sessionId);
+    @WebMethod void logOut(byte [] sessionId);
 
 }
